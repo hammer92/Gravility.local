aa0c32915e0da649a4c9f591062ce59b506cc063
hammer92 <hammer92@hotmail.es>
2015-11-22 13:38:13 -0500

    Preguntas

--- resources/views/gravility/preguntas.blade.php
+++ resources/views/gravility/preguntas.blade.php
@@ -1,4 +1,18 @@
 @extends('gravility.menu')
  @section('cantenido')
+<h3>1. ¿ En qué consiste el principio de responsabilidad única ? ¿Cual es su propósito?</h3>
+<pre class="pre-scrollable">
+	
+</pre>
 
+<h3>2. ¿Que características tiene según su opinión “buen” código o código limpio?</h3>
+<pre class="pre-scrollable">
+	Para mi un código limpio tiene que tener las siguientes características:
+		1. Buena tabulación
+		2. Comentarios claros de lo que se realiza
+		3. Las funciones deben ser reutilizables.
+		4. Aplicar POO
+		5. tener muy claro qué tipo de metodología de desarrollo se piensa utilizar
+
+</pre>
 @stop
